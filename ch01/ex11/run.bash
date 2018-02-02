ae1cc9a315781015ce7f8a3d9b0148168da27601
shoito <shoito@users.noreply.github.com>
2018-02-02 21:53:06 +0900

    impl ch01/ex11

---
+++ ch01/ex11/run.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# alexa.com にある上位 100 万件のウェブサイト*2のように、より長い引数リストで fetchall を試しなさい。
+# あるウェブサイトが応答しない場合には、プログラムはどのように振る舞うでしょうか。
+# (そのような場合に対処するための機構は 8.9 節で説明されています。)
+
+go run ../ex10/main.go http://urlnotfou.nd http://google.com http://youtube.com http://facebook.com http://baidu.com http://wikipedia.org http://reddit.com http://yahoo.com http://google.co.in http://qq.com http://amazon.com http://taobao.com http://twitter.com http://tmall.com http://google.co.jp http://live.com http://vk.com http://instagram.com http://sohu.com http://sina.com.cn http://jd.com http://weibo.com http://360.cn http://google.de http://google.co.uk http://google.com.br http://google.fr http://google.ru http://yandex.ru http://netflix.com http://linkedin.com http://twitch.tv http://google.it http://t.co http://google.com.hk http://google.es http://pornhub.com http://list.tmall.com http://alipay.com http://xvideos.com http://google.ca http://yahoo.co.jp http://ebay.com http://google.com.mx http://microsoft.com http://bing.com http://imgur.com http://ok.ru http://bongacams.com http://office.com http://aliexpress.com
