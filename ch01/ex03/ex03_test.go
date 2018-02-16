8d8c020f0895bf5b630dd443e2ea6033fe0bb823
shoito <shoito@users.noreply.github.com>
2018-02-16 11:40:56 +0900

    ch01/ex03のリファクタリング
    - b.ResetTimer()はタイマーを止めて行う初期処理がないため不要
    - ベンチマークテストするにはarrの文字列数が足りないので追加

--- ch01/ex03/ex03_test.go
+++ ch01/ex03/ex03_test.go
@@ -9,16 +9,12 @@ import (
 )
 
 func BenchmarkForLoop(b *testing.B) {
-	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		ForLoop()
 	}
 }
 
 func BenchmarkStringsJoin(b *testing.B) {
-	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		StringsJoin()
 	}
