cd78a4fb301fdb20087501a945e0ac2f40b3a2d6
shoito <shoito@users.noreply.github.com>
2018-03-16 11:27:28 +0900

    ベンチマークのSSA対応

--- ch02/ex04/ex04_test.go
+++ ch02/ex04/ex04_test.go
@@ -4,14 +4,21 @@ import (
 	"testing"
 )
 
+var input uint64 = 0x1234567890ABCDEF
+var output int
+
 func BenchmarkPopCount(b *testing.B) {
+	var c int
 	for i := 0; i < b.N; i++ {
-		PopCount(0x1234567890ABCDEF)
+		c = PopCount(input)
 	}
+	output = c
 }
 
 func BenchmarkPopCountByShift(b *testing.B) {
+	var c int
 	for i := 0; i < b.N; i++ {
-		PopCountByShift(0x1234567890ABCDEF)
+		c = PopCountByShift(input)
 	}
+	output = c
 }
