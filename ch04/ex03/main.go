aa49c05212ea53b269a6b87381b6ab81ff3da113
shoito <shoito@users.noreply.github.com>
2018-05-17 00:35:20 +0900

    impl ch04/ex03

---
+++ ch04/ex03/main.go
@@ -0,0 +1,17 @@
+// スライスの代わりに配列へのポインタを使うようにreverseを書き換えなさい。
+
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(nums *[8]int) {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+}
